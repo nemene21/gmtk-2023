9d76007d15f501530d6a6877c00b3b9bf7559f7a
ne_mene21 <73829851+nemene21@users.noreply.github.com>
2023-07-07 20:06:35 +0200

    more items

--- items.gd
+++ items.gd
@@ -7,8 +7,20 @@ const items = {
 			"damage_per_bullet" : 0.1
 		},
 		"description" : "The more friendly bullets there are, the more damage you do!"
+	},
+	"Bouncy Substance" : {
+		"stats" : {
+			"bullet_bounce" : 1
+		},
+		"description" : "Your bullets bounce off the screens edges!"
+	},
+	"Patient Demise" : {
+		"stats" : {
+			"bullet_slowdown" : 1
+		},
+		"description" : "Bullets will patiently wait to hit something instead of going off-screen!"
 	}
 }
 
-static func new_item(name):
+static func new_item(name : String) -> Dictionary:
 	return items[name].duplicate(true)
