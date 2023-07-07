547d9b902b611924d81a7babf482bd6857ab1189
ne_mene21 <73829851+nemene21@users.noreply.github.com>
2023-07-07 20:14:59 +0200

    more items

--- items.gd
+++ items.gd
@@ -12,15 +12,33 @@ const items = {
 		"stats" : {
 			"bullet_bounce" : 1
 		},
-		"description" : "Your bullets bounce off the screens edges!"
+		"description" : "Your bullets bounce off the screen's edges!"
 	},
 	"Patient Demise" : {
 		"stats" : {
 			"bullet_slowdown" : 1
 		},
 		"description" : "Bullets will patiently wait to hit something instead of going off-screen!"
+	},
+	"Silly Shells" : {
+		"stats" : {
+			"bullet_amount" : 2,
+			"spread" : 33
+		},
+		"description" : "You shoot 2 more bullets but loose accuracy..."
+	},
+	"Sniper Scope" : {
+		"stats" : {
+			"bullet_speed" : 600,
+			"spread" : -33,
+			"firerate" : -0.5,
+			"damage" : 10
+		},
+		"description" : "You shoot 2 more bullets but loose accuracy..."
 	}
 }
 
 static func new_item(name : String) -> Dictionary:
-	return items[name].duplicate(true)
+	var item = items[name].duplicate(true)
+	item.name = name
+	return item
