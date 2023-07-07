532eb12b2820b7e7bcee586eac8e44106018b422
ne_mene21 <73829851+nemene21@users.noreply.github.com>
2023-07-07 20:02:04 +0200

    first item

--- items.gd
+++ items.gd
@@ -2,8 +2,13 @@ extends Node
 class_name Items
 
 const items = {
-	""
+	"Allmighty Orb" : {
+		"stats" : {
+			"damage_per_bullet" : 0.1
+		},
+		"description" : "The more friendly bullets there are, the more damage you do!"
+	}
 }
 
 static func new_item(name):
-	pass
+	return items[name].duplicate(true)
