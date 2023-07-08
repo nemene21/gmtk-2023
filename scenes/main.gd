7a9b18c5d30e5aa561545e5cac60785c6d989587
iamcheeseman <xanderswan32@gmail.com>
2023-07-08 14:12:04 -0400

    Hole vignette

--- scenes/main.gd
+++ scenes/main.gd
@@ -49,6 +49,14 @@ func new_wave():
 			
 	wave += 1
 
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("fullscreen"):
+		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_WINDOWED:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
 func enemy_died():
 	enemy_count -= 1
 	if enemy_count == 0:
