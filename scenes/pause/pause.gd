8069a1570f3850b3b2dc1d37974192b026788444
iamcheeseman <xanderswan32@gmail.com>
2023-07-08 19:13:49 -0400

    Polished pause menu

--- scenes/pause/pause.gd
+++ scenes/pause/pause.gd
@@ -1,9 +1,26 @@
 extends Control
 
+@onready var bg = $ColorRect
+
 func _ready() -> void:
 	visible = false
 
+func toggle_pause() -> void:
+	visible = !visible
+	get_tree().paused = visible
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
-		visible = !visible
-		get_tree().paused = visible
+		if !visible:
+			toggle_pause()
+			position.y = bg.size.y
+			var tween = create_tween()
+			tween.set_trans(Tween.TRANS_ELASTIC)
+			tween.set_ease(Tween.EASE_OUT)
+			tween.tween_property(self, "position", Vector2(0, 0), 1)
+		else:
+			var tween = create_tween()
+			tween.set_trans(Tween.TRANS_EXPO)
+			tween.set_ease(Tween.EASE_IN)
+			tween.tween_property(self, "position", Vector2(0, bg.size.y), 0.2)
+			tween.tween_callback(toggle_pause)
