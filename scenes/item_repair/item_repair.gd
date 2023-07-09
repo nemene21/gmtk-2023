bcf0ee7bf51aad9a686cdd0db3ba7313e0237f39
iamcheeseman <xanderswan32@gmail.com>
2023-07-08 20:15:31 -0400

    Only add items you can repair

--- scenes/item_repair/item_repair.gd
+++ scenes/item_repair/item_repair.gd
@@ -1,10 +1,15 @@
 extends Control
 
 @onready var bg = $ColorRect
+@onready var info = %Info
 @onready var item_lost_label = %ItemLost
 @onready var items_container = %Items
 
+var label_settings : LabelSettings
+
 func _ready() -> void:
+	label_settings = LabelSettings.new()
+	label_settings.font_color = Color.YELLOW
 	visible = false
 
 func unpause() -> void:
@@ -19,20 +24,12 @@ func close() -> void:
 	tween.tween_callback(unpause)
 
 func repair_item(button : Button, item_name : String) -> void:
-	var cost = Items.items[item_name].repair_cost
-	if Global.player.money - cost < 0:
-		button.add_theme_color_override("font_color", Color.RED)
-		button.add_theme_color_override("font_pressed_color", Color.RED)
-		button.add_theme_color_override("font_hover_color", Color.RED)
-		button.add_theme_color_override("font_focus_color", Color.RED)
-		return
-	
 	close()
 	
 	var player_data = Global.player.player_data
 	player_data.voided_items.erase(item_name)
 	player_data.add_item(item_name)
-	Global.player.money -= cost
+	Global.player.money -= Items.items[item_name].repair_cost
 	
 	player_data.emit_signal("items_changed")
 	Global.player.emit_signal("gain_money", Global.player.money)
@@ -45,12 +42,22 @@ func update(voided_item : String) -> void:
 			button.queue_free()
 	for item_name in player_data.voided_items:
 		var item = Items.items[item_name]
+		var cost = item.repair_cost
+		if Global.player.money - cost < 0:
+			continue
 		var button = Button.new()
 		button.text = "%s for %d" % [item_name.to_lower(), item.repair_cost]
 		button.icon = item.texture
 		button.pressed.connect(repair_item.bind(button, item_name))
 		items_container.add_child(button)
 		items_container.move_child(button, 0)
+	
+	if items_container.get_child_count() == 1:
+		var warning = Label.new()
+		warning.label_settings = label_settings
+		warning.text = "you have no items you can repair"
+		warning.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+		info.add_child(warning)
 
 func update_and_show(voided_item : String) -> void:
 	update(voided_item)
